e824dc842bc63b2fca53a1a022f29d17b7b92722
ooonush <ooonush@gmail.com>
2022-09-26 20:40:42 +0500

    Enterprise Refactoring

--- Incapsulation/EnterpriseTask/Enterprise.cs
+++ Incapsulation/EnterpriseTask/Enterprise.cs
@@ -1,61 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Incapsulation.EnterpriseTask
 {
     public class Enterprise
     {
-        Guid guid;
-
-        public Guid getGuid() { return guid; }
-
+        public readonly Guid Guid;
+        
+        public string Name { get; set; }
+        public DateTime EstablishDate { get; set; }
+        
+        string _inn;
+        public string Inn
+        {
+            get => _inn;
+            set
+            {
+                if (value.Length != 10 || !value.All(char.IsDigit))
+                    throw new ArgumentException();
+                _inn = value;
+            }
+        }
+        
         public Enterprise(Guid guid)
         {
-            this.guid = guid;
+            Guid = guid;
         }
+        
+        public TimeSpan ActiveTimeSpan => DateTime.Now - EstablishDate;
 
-        string name;
-
-        public string getName() { return name; }
-
-        public void setName(string name) { this.name = name; }
-
-        string inn;
-
-        public string getINN() { return inn; }
-
-        public void setINN(string inn)
-        {
-            if (inn.Length != 10 || !inn.All(z => char.IsDigit(z)))
-                throw new ArgumentException();
-            this.inn = inn;
-        }
-
-        DateTime establishDate;
-
-        public DateTime getEstablishDate()
-        {
-            return establishDate;
-        }
-
-        public void setEstablishDate(DateTime establishDate)
-        {
-            this.establishDate = establishDate;
-        }
-
-        public TimeSpan getActiveTimeSpan()
-        {
-            return DateTime.Now - establishDate;
-        }
-
-        public double getTotalTransactionsAmount()
+        public double GetTotalTransactionsAmount()
         {
             DataBase.OpenConnection();
             var amount = 0.0;
-            foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == guid))
+            foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == Guid))
                 amount += t.Amount;
             return amount;
         }
