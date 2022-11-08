3e89cf86b52fd5125928055aeaec4ba4ff07d805
Анушервон <ooonush@gmail.com>
2022-11-08 15:03:05 +0500

    TreeTraversal todo

---
+++ Practices/Delegates/TreeTraversal/Traversal.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.TreeTraversal
+{
+    public static class Traversal
+    {
+        public static IEnumerable<Product> GetProducts(ProductCategory root)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Job> GetEndJobs(Job root)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
