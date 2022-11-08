a33160f41b13d1ac01d51be88b226250b725f794
Анушервон <ooonush@gmail.com>
2022-11-08 13:35:47 +0500

    Reports refactoring

--- Practices/Delegates/Reports/Measurement.cs
+++ Practices/Delegates/Reports/Measurement.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Delegates.Reports
+﻿namespace Delegates.Reports
 {
     public class Measurement
     {
         public double Temperature { get; set; }
         public double Humidity { get; set; }
-
     }
 }
--- Practices/Delegates/Reports/ReportMaker_should.cs
+++ Practices/Delegates/Reports/ReportMaker_should.cs
@@ -32,32 +32,32 @@ namespace Delegates.Reports
 		[Test]
 		public void MeanAndStdHtml()
 		{
-			var expected = @"<h1>Mean and Std</h1><ul><li><b>Temperature</b>: 9±17.0880074906351<li><b>Humidity</b>: 2±0.816496580927726</ul>";
-			var actual = ReportMakerHelper.MeanAndStdHtmlReport(data);
+			string expected = @"<h1>Mean and Std</h1><ul><li><b>Temperature</b>: 9±17.0880074906351<li><b>Humidity</b>: 2±0.816496580927726</ul>";
+			string actual = ReportMakerHelper.MeanAndStdHtmlReport(data);
 			Assert.AreEqual(expected, actual);
 		}
 
 		[Test]
 		public void MedianMarkdown()
 		{
-			var expected = "## Median\n\n * **Temperature**: 8\n\n * **Humidity**: 2\n\n";
-			var actual = ReportMakerHelper.MedianMarkdownReport(data);
+			string expected = "## Median\n\n * **Temperature**: 8\n\n * **Humidity**: 2\n\n";
+			string actual = ReportMakerHelper.MedianMarkdownReport(data);
 			Assert.AreEqual(expected, actual);
 		}
 
 		[Test(Description = "Новый тест, который нужно сделать рабочим после рефакторинга")]
 		public void MeanAndStdMarkdown()
 		{
-			var expected = "## Mean and Std\n\n * **Temperature**: 9±17.0880074906351\n\n * **Humidity**: 2±0.816496580927726\n\n";
-			var actual = ReportMakerHelper.MeanAndStdMarkdownReport(data);
+			string expected = "## Mean and Std\n\n * **Temperature**: 9±17.0880074906351\n\n * **Humidity**: 2±0.816496580927726\n\n";
+			string actual = ReportMakerHelper.MeanAndStdMarkdownReport(data);
 			Assert.AreEqual(expected, actual);
 		}
 
 		[Test(Description = "Новый тест, который нужно сделать рабочим после рефакторинга")]
 		public void MedianHtml()
 		{
-			var expected = "<h1>Median</h1><ul><li><b>Temperature</b>: 8<li><b>Humidity</b>: 2</ul>";
-			var actual = ReportMakerHelper.MedianHtmlReport(data);
+			string expected = "<h1>Median</h1><ul><li><b>Temperature</b>: 8<li><b>Humidity</b>: 2</ul>";
+			string actual = ReportMakerHelper.MedianHtmlReport(data);
 			Assert.AreEqual(expected, actual);
 		}
 	}
