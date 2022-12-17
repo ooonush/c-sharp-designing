3493be1c4ef6b1b2a6ce6dd825b359939bca8f7b
Анушервон <ooonush@gmail.com>
2022-12-17 14:32:22 +0500

    DDD Taxi Task

---
+++ Practices/DDD/Taxi/Infrastructure/ValueType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Ddd.Infrastructure
+{
+	/// <summary>
+	/// Базовый класс для всех Value типов.
+	/// </summary>
+	public class ValueType<T>
+	{
+	}
+}
