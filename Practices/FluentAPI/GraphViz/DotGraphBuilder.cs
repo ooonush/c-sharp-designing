4c2879053bd27ce2311cd3835c2dd199c78058d9
Анушервон <ooonush@gmail.com>
2022-12-17 19:23:19 +0500

    FluentAPI GraphViz Task

---
+++ Practices/FluentAPI/GraphViz/DotGraphBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace FluentApi.Graph
+{
+	/*
+	public class DotGraphBuilder
+	{
+		public static ... DirectedGraph(string graphName)
+		{
+			return ...
+		}
+
+	}
+	*/
+}
